057b40f5ff2337cfb3a7bd8e6b149f9c702a6e90
nekovalue <nekovalue@gmail.com>
2021-10-31 10:55:44 +0200

    sym_counter()

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include "stdafx.h"
 
+int sym_counter()
+{
+    char word[10] = "console", sym = 'o';
+    int count = 0;
+
+    char* pw;
+
+    for (pw = word; *pw; ++pw)
+    {
+        if (*pw == sym) ++count;
+    }
+
+    printf("\nThere is %d symbols `%c` in word `%s`\n", count, sym, word);
+    return 0;
+}
+
 int main() {
-    printf("Lab04\n");
+
     return 0;
 }
